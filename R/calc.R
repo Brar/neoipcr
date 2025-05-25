8290059a19dbbcfba15bdb407092d482accf6fa3
Brar Piening <brar@gmx.de>
2025-05-25 16:56:51 +0200

    Add functions to calculate birthweight categories

--- R/calc.R
+++ R/calc.R
@@ -1558,6 +1558,62 @@ get_procedure_category_pretty <- function(x)
   )
 }
 
+bw50 <- function(x, as_factor = TRUE)
+{
+  m <- floor((x-25)/50)*50+50
+  if(!as_factor)
+    return(m)
+
+  lb <- m-25
+  ub <- m+24
+  ordered(
+    m,
+    levels = seq(min(m), max(m), 50),
+    labels = paste0(format(seq(min(lb), max(lb), 50))," g - ",format(seq(min(ub), max(ub), 50))," g"))
+}
+
+bw125 <- function(x, as_factor = TRUE)
+{
+  m <- floor((x-63)/125)*125+125
+  if(!as_factor)
+    return(m)
+
+  lb <- m-62
+  ub <- m+62
+  ordered(
+    m,
+    levels = seq(min(m), max(m), 125),
+    labels = paste0(format(seq(min(lb), max(lb), 125))," g - ",format(seq(min(ub), max(ub), 125))," g"))
+}
+
+bw250 <- function(x, as_factor = TRUE)
+{
+  m <- floor((x-125)/250)*250+250
+  if(!as_factor)
+    return(m)
+
+  lb <- m-125
+  ub <- m+124
+  ordered(
+    m,
+    levels = seq(min(m), max(m), 250),
+    labels = paste0(format(seq(min(lb), max(lb), 250))," g - ",format(seq(min(ub), max(ub), 250))," g"))
+}
+
+bw500 <- function(x, as_factor = TRUE)
+{
+  m <- as.integer(x/500)*500+250
+  if(!as_factor)
+    return(m)
+
+  lb <- m-250
+  ub <- m+249
+  ordered(
+    m,
+    levels = seq(min(m), max(m), 500),
+    labels = paste0(format(seq(min(lb), max(lb), 500))," g - ",format(seq(min(ub), max(ub), 500))," g"))
+}
+
 add_class <- function(x, class_name)
 {
   check_string(class_name, allow_empty = FALSE)
