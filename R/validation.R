3b346292556512a61867e0ec62ac457c9e1408f0
Brar Piening <brar.piening@charite.de>
2025-08-07 15:02:52 +0200

    Implement rule 18

--- R/validation.R
+++ R/validation.R
@@ -818,12 +818,44 @@ validation_rule_18 <- function(x, exceptions)
 {
   check_neoipcr_ds(x)
 
-  # TODO: Implement
   r <- dplyr::bind_cols(
     rule_id = c(18L),
     x$enrollments |>
-      dplyr::select("enrollment_key") |>
-      dplyr::filter(.data$enrollment_key == -1))
+      dplyr::select(
+        tidyselect::any_of(c("hospital_key","department_key","patient_key")),
+        "enrollment_key","enrolledAt") |>
+      dplyr::inner_join(
+        x$events |>
+          dplyr::inner_join(
+            x$surveillanceEndData |>
+              dplyr::select("event_key", "patient_days"),
+            dplyr::join_by("event_key")) |>
+          dplyr::select("enrollment_key","occurredAt","patient_days"),
+        dplyr::join_by("enrollment_key")) |>
+      dplyr::mutate(
+        patient_days_calculated = 1L + as.integer(.data$occurredAt - .data$enrolledAt)
+      ) |>
+      dplyr::filter(.data$patient_days_calculated != .data$patient_days) |>
+      dplyr::select(
+        tidyselect::any_of(
+          c("hospital_key",
+            "department_key",
+            "patient_key")),"enrollment_key","enrolledAt","occurredAt",
+        "patient_days","patient_days_calculated")) |>
+    dplyr::group_by(dplyr::across(!c("enrolledAt","occurredAt","patient_days","patient_days_calculated"))) |>
+    dplyr::summarise(
+      context = list(
+        list(
+          enrolledAt = .data$enrolledAt,
+          occurredAt = .data$occurredAt,
+          patient_days = .data$patient_days,
+          patient_days_calculated = .data$patient_days_calculated)))
+
+  if(!is.null(exceptions))
+    r <- r |>
+    dplyr::anti_join(
+      exceptions,
+      dplyr::join_by("rule_id","enrollment_key"))
 
   return(r)
 }
